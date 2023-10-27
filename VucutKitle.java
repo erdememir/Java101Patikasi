7a5e2003437b84d91b3a572a8a11762347de42ff
erdememir <146975617+erdememir@users.noreply.github.com>
2023-10-27 20:41:52 +0300

    Create VucutKitle.java

---
+++ VucutKitle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class VucutKitle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double kg;
+        double boy;
+        
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kg = input.nextDouble();
+        
+        double vke = kg / (boy * boy);
+        System.out.print("Vücut kitle endeksiniz: " + vke);
+    }    
+}
