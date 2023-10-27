d449ae98fda36fdc9ca3e88a7320692becc1e033
erdememir <146975617+erdememir@users.noreply.github.com>
2023-10-27 20:59:35 +0300

    Create Manav.java

---
+++ Manav.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Manav {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double armut = 2.14;
+        double elma = 3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican = 5.00;
+        double kgArmut, kgElma, kgDomates, kgMuz, kgPatlican;
+        
+        System.out.print("Armut kaç kilo?: ");
+        kgArmut = input.nextDouble();
+        
+        System.out.print("Elma kaç kilo?: ");
+        kgElma = input.nextDouble();
+        
+        System.out.print("Domates kaç kilo?: ");
+        kgDomates = input.nextDouble();
+        
+        System.out.print("Muz kaç kilo?: ");
+        kgMuz = input.nextDouble();
+        
+        System.out.print("Patlıcan kaç kilo?: ");
+        kgPatlican = input.nextDouble();
+        
+        double tutar = (armut*kgArmut)+(elma*kgElma)+(domates*kgDomates)+(muz*kgMuz)+(patlican*kgPatlican);
+        System.out.print("Toplam Tutar: "+tutar+" TL");
+        
+    }
+}
