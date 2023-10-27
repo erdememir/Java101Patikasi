cc2ecce63add03c889e0c8612aeb79d6d8511928
erdememir <146975617+erdememir@users.noreply.github.com>
2023-10-27 20:00:40 +0300

    Create DaireDilimAlani.java

---
+++ DaireDilimAlani.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Daire2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        double r;
+        double a;
+        double pi = 3.14;
+        
+        System.out.print("Dairenin yarıçapını giriniz: ");
+        r = input.nextDouble();
+        System.out.print("Dairenin merkez açısının ölçüsünü giriniz: ");
+        a = input.nextDouble();
+        
+        double dilimAlan = (pi * (r*r) * a);
+        
+        System.out.print("Dairede belirtilen dilimin alanı: "+ dilimAlan);
+    }
+}
