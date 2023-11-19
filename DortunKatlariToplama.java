01834c9c1bb84b18c2c662fa2e9e817a8ae95903
erdememir <146975617+erdememir@users.noreply.github.com>
2023-11-19 10:35:08 +0300

    Create DortunKatlariToplama.java

---
+++ DortunKatlariToplama.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class DortunKatlariToplama {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i;
+        int total = 0;
+        do {
+            System.out.print("Sayı giriniz: ");
+            i = input.nextInt();
+            if (i % 4 == 0) {
+                total += i;
+            }
+        } while (i % 2 == 0);
+        System.out.println("Sonuç: " + total);
+    }
+}
