8a5cadd1e514051e4ef9a06c60b3ea19ef3620af
erdememir <146975617+erdememir@users.noreply.github.com>
2023-11-20 11:25:13 +0300

    Create DortBesKuvvet.java

---
+++ Homework/DortBesKuvvet.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class DortBesKuvvet {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k;
+        System.out.print("Sınır sayı: ");
+        k = input.nextInt();
+        for (int i = 1; i <= k; i *= 4) {
+            System.out.print(i + " ");
+        }
+        for (int i = 1; i <= k; i *= 5) {
+            System.out.print(i + " ");
+        }
+    }
+}
