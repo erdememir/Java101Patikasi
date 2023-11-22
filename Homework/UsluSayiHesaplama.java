518bc82984e24c6495c3624f60ce3bb49f9a8800
erdememir <146975617+erdememir@users.noreply.github.com>
2023-11-23 00:44:56 +0300

    Create UsluSayiHesaplama.java

---
+++ Homework/UsluSayiHesaplama.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class UsluSayiHesaplama {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sayi;
+        int ust;
+        int total = 1;
+
+        System.out.print("Üssü hesaplanacak sayı: ");
+        sayi = input.nextInt();
+        System.out.print("Üst sayısı: ");
+        ust = input.nextInt();
+        for (int i = 1; i <= ust; i++) {
+            total *= sayi;
+        }
+        System.out.println("Cevap: " + total);
+
+    }
+}
