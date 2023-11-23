01266855f1576836bc174e08c32d2bdcea78b987
erdememir <146975617+erdememir@users.noreply.github.com>
2023-11-23 03:15:18 +0300

    Create BasamakSayilariToplami.java

---
+++ Homework/BasamakSayilariToplami.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class BasamakSayilariToplami {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number;
+        int result = 0;
+        int value;
+
+        System.out.print("Basamakları toplanması istenen sayı: ");
+        number = input.nextInt();
+
+        while (number != 0) {
+            value = number % 10;
+            result += value;
+            number /= 10;
+        }
+
+        System.out.println("Toplam: " + result);
+    }
+}
