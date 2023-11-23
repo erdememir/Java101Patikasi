4b66957a419774872962b896920f43daec48d221
erdememir <146975617+erdememir@users.noreply.github.com>
2023-11-24 01:02:53 +0300

    Create HarmonikSayi.java

---
+++ Homework/HarmonikSayi.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class HarmonikSayi {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.print("\"n\" sayısı: ");
+        n = input.nextInt();
+        double result = 0;
+        for (double i = 1; i <= n; i++) {
+            result += 1 / i;
+        }
+        System.out.println("Sonuç: " + result);
+    }
+}
