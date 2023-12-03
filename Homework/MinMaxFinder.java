5f85e0c82b4f8e67041411c89099db1dac6b3e81
erdememir <146975617+erdememir@users.noreply.github.com>
2023-12-03 18:32:35 +0300

    Create MinMaxFinder.java

---
+++ Homework/MinMaxFinder.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class MinMaxFinder {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Gireceğiniz sayı adeti: ");
+        int n = input.nextInt();
+        int maxValue = 1;
+        int minValue = 1;
+        int current;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + ". sayıyı giriniz: ");
+            current = input.nextInt();
+            if (i == 1) {
+                maxValue = current;
+                minValue = current;
+            }
+            if (current > maxValue) {
+                maxValue = current;
+            } else if (current < minValue) {
+                minValue = current;
+            }
+        }
+        System.out.println("En küçük sayı: " + minValue + "\nEn büyük sayı: " + maxValue);
+    }
+}
