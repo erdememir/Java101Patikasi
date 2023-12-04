08993e0772196e303b70d7dc407d5a3508082b00
erdememir <146975617+erdememir@users.noreply.github.com>
2023-12-04 18:37:01 +0300

    Create MukemmelSayi.java

---
+++ Homework/MukemmelSayi.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class MukemmelSayi {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı giriniz: ");
+        int n = input.nextInt();
+        int toplam = 0;
+
+        for (int i = n; i > 0; i--) {
+            if (n % i == 0) {
+                toplam += i;
+            }
+        }
+        if ((toplam - n) == n) {
+            System.out.println(n + " Mükemmel bir sayıdır.");
+        } else {
+            System.out.println(n + " Mükemmel bir sayı değildir.");
+        }
+    }
+}
