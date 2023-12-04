59a10f03a57a4c54f5d0f30f9ade9319e7a63412
erdememir <146975617+erdememir@users.noreply.github.com>
2023-12-04 20:08:07 +0300

    Create TersUcgen.java

---
+++ Homework/TersUcgen.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class TersUcgen {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak sayısı giriniz: ");
+        int n = input.nextInt();
+        int z = 0;
+        int k = n * 2 - 1;
+        for (int x = 1; x <= n; x++) {
+            z++;
+            for (int y = 1; y <= z - 1; y++) {
+                System.out.print(" ");
+            }
+            for (int u = 1; u <= k; u++) {
+                System.out.print("*");
+            }
+            k -= 2;
+            System.out.println();
+        }
+    }
+}
