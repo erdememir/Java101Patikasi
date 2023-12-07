d16868adba9f9cb6db2775c28b5941e5583c431b
erdememir <146975617+erdememir@users.noreply.github.com>
2023-12-07 18:28:58 +0300

    Create Fibonacci.java

---
+++ Homework/Fibonacci.java
@@ -0,0 +1,19 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Fibonacci serisi bulan program.");
+        System.out.print("İstenilen eleman sayısı: ");
+        int n = input.nextInt();
+        int x = 0, y = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(x + " ");
+            y = x + y;
+            x = y - x;
+        }
+    }
+}
