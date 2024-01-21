75510e51cbb9a471be0ed4fd5be8b29580066d9b
emir-iş <emrhnerdm41@gmail.com>
2024-01-21 12:33:13 +0300

    hackerrank solution completed

--- Homework/Solution.java
+++ Homework/Solution.java
@@ -1,5 +1,6 @@
 package Homework;
 
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class Solution {
@@ -11,26 +12,31 @@ public class Solution {
         String zStr = input.nextLine();
 
         String[] wordsX = xStr.split(" ");
+        int stringSizeX = wordsX[0].length();
 
         System.out.println("================================");
         System.out.print(wordsX[0]);
-        for (int j = 1; j <= 15; j++) {
+        for (int j = 1; j <= 15 - stringSizeX; j++) {
             System.out.print(" ");
         }
         System.out.println(getInt(wordsX[1]));
 
 
         String[] wordsY = yStr.split(" ");
+        int stringSizeY = wordsY[0].length();
+
         System.out.print(wordsY[0]);
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 15 - stringSizeY; i++) {
             System.out.print(" ");
         }
         System.out.println(getInt(wordsY[1]));
 
 
         String[] wordsZ = zStr.split(" ");
+        int stringSizeZ = wordsZ[0].length();
+
         System.out.print(wordsZ[0]);
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 15 - stringSizeZ; i++) {
             System.out.print(" ");
         }
         System.out.println(getInt(wordsZ[1]));
@@ -41,7 +47,7 @@ public class Solution {
 
     static String getInt(String word) {
         int stringSizeX = word.length();
-        if (stringSizeX < 3 && stringSizeX > 1) {
+        if (stringSizeX == 2) {
             word = "0" + word;
         } else if (stringSizeX < 2) {
             word = "00" + word;
